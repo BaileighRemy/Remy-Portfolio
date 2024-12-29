b635ca177cbe42b07d2606293b8e0e02bb34fe57
Baileigh Remy <baileigh.remy@gmail.com>
2024-12-28 22:32:17 -0600

    Added portfolio and project pictures

--- 02-Challenge/remy-portfolio/src/AboutMe.jsx
+++ 02-Challenge/remy-portfolio/src/AboutMe.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ProfilePic from './assets/ProfilePic.jpg';
 
 const AboutMe = () => {
 
     return (
         <div className="AboutMe">
            <h1 className="about-title">About Me</h1>
-            <img className="profile-pic" src="path/to/your/photo.jpg" alt="Baileigh Remy" />
+            <img className="profile-pic" src={ProfilePic} alt="Baileigh Remy" />
             <p className="bio">
                 Hello! I'm Baileigh Remy, a full stack web developer with experience in HTML, CSS, Javascript, Node.js, React and more! I love problem solving and creating dynamic websites. In my freetime I enjoy learning foreign languages, travelling, reading, playing videogames and anything outdoors. 
             </p>
--- 02-Challenge/remy-portfolio/src/Portfolio.jsx
+++ 02-Challenge/remy-portfolio/src/Portfolio.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import image1 from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.jpg';
+import image4 from './assets/image4.jpg';
+import image5 from './assets/image5.jpg';
+import image6 from './assets/image6.jpg';
+
 
 const Portfolio = () => {
     const projectList = [
@@ -7,45 +14,45 @@ const Portfolio = () => {
             description: "Provides current weather and 5 day forecast based on city searched.",
             deployedLink: "https://weather-dashboard-6mqh.onrender.com/",
             githubLink: "https://github.com/BaileighRemy/Weather-Dashboard.git",
-            image: "path/to/image1.jpg" // Replace with actual image path
+            image: image1 
         },
         {
             title: "Vehicle Builder",
             description: "Node.js application that allows users to build a vehicle.",
             deployedLink: "https://app.screencastify.com/v3/watch/mCOiz9Mmt6ilAfiXuzYA",
             githubLink: "https://github.com/BaileighRemy/Vehicle_Builder",
-            image: "path/to/image2.jpg" // Replace with actual image path
+            image: image2
         },
         {
             title: "Survey Form",
             description: "Survey Form for a Coding Bootcamp",
             deployedLink: "https://baileighremy.github.io/survey-form/homepage.html",
             githubLink: "https://github.com/BaileighRemy/survey-form.git",
-            image: "path/to/image3.jpg" // Replace with actual image path
+            image: image3
         },
         {
             title: "README Generator",
             description: "Node.js application that generates a README file based on user input.",
             deployedLink: "https://watch.screencastify.com/v/ltq9Xd92PbJeduWor50O",
             githubLink: "https://github.com/BaileighRemy/README-Generator/tree/main/Develop",
-            image: "path/to/image3.jpg" // Replace with actual image path
+            image: image4
         },
         {
             title: "Prework Study Guide",
             description: "Study guide for basics of HTML, CSS, Git, and Javascript.",
             deployedLink: "https://baileighremy.github.io/prework-study-guide/",
             githubLink: "https://github.com/BaileighRemy/prework-study-guide",
-            image: "path/to/image3.jpg" // Replace with actual image path
+            image: image5
         },
         {
             title: "Employee Tracker",
             description: "Node.js application that tracks employees, roles, and departments.",
             deployedLink: "https://github.com/BaileighRemy/Employee-Tracker/tree/main/02-Challenge",
             githubLink: "https://github.com/BaileighRemy/Employee-Tracker/tree/main/02-Challenge",
-            image: "path/to/image3.jpg" // Replace with actual image path
+            image: image6
         },
         
-        // Add more projects as needed
+        
     ];
 
     return (
