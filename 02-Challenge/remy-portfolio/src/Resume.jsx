057d8126d82fccb2a6b7e3949b7a28548aa9eca5
Baileigh Remy <baileigh.remy@gmail.com>
2024-12-28 22:40:58 -0600

    Added Resume information

--- 02-Challenge/remy-portfolio/src/Resume.jsx
+++ 02-Challenge/remy-portfolio/src/Resume.jsx
@@ -2,8 +2,19 @@ const Resume = () => {
 
     return (
         <div className="resume">
-            <h2>Welcome to my portfolio!</h2>
-            <p>Click on the links above to learn more about me, view my portfolio, contact me, or view my resume.</p>
+            <h2>Resume</h2>
+            <p>Download my resume: <a href="../public/RemyResume.pdf" target="_blank" rel="noopener noreferrer">Resume.pdf</a></p>
+            <h3>Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>Postgres</li>
+                <li>Git & GitHub</li>
+            </ul>
         </div>
     );
 }
